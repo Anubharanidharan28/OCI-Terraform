b4b7f46ee880cb2464159f152be4492aecd7e90a
Anubharanidharan M <in04180@K4T02CWX7T.local>
2025-09-06 11:24:31 +0530

    backend.tf

--- terraform-folder/backend.tf
+++ terraform-folder/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket  = "oci-terraform-tfstate-bucket"
     key     = "../terraform.tfstate" // bucket file path
-    region  = "me-central-1" 
+    region  = ""
     encrypt = true
   }
 }
